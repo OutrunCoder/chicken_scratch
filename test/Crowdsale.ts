d72985fece21aa2db44fd5468d86f2792390696c
David Pineda <david.pineda@deparadise.com>
2023-02-02 22:43:02 -0500

    Crowdsale initial test setup

--- test/Crowdsale.ts
+++ test/Crowdsale.ts
@@ -1,7 +1,18 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import Convert from '../utils/token-conversion';
+// import Convert from '../utils/token-conversion';
 
 describe('Crowdsale', () => {
+  let crowdsaleContract: any;
   
+  beforeEach(async() => {
+    const contractFactory = await ethers.getContractFactory('Crowdsale');
+    crowdsaleContract = await contractFactory.deploy();
+  });
+
+  describe('Deployment', () => {
+    it('Has correct name', async() => {
+      expect(await crowdsaleContract.name()).to.equal('Crowdsale');
+    });
+  });
 })
