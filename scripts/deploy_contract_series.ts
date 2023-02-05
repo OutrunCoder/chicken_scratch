bd21555f5bd364f85eedf1ee1b93cd63a25ea58f
David Pineda <david.pineda@deparadise.com>
2023-02-05 17:48:54 -0500

    pseudo code deploy_contract_series game plan.

---
+++ scripts/deploy_contract_series.ts
@@ -0,0 +1,15 @@
+// STAGE 0
+// - accept custom params - via main
+// - establish a deployment series config w/ meta
+// STAGE 1
+// - map on config serries
+  // - collect contract
+  // - Deploy w/ config params
+  // - confirm address via console
+  // next...
+// STAGE 3
+// - Call Business logic on contracts if required
+// - confirm business logic has been executed
+
+// - create shell script
+// - create/add documentation
