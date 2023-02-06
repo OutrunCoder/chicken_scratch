095bed9c0b95fecd9f6c5fb19263d3bc7059bb2d
David Pineda <david.pineda@deparadise.com>
2023-02-06 07:08:31 -0500

    First setup of the deploymentConfig

--- scripts/deploy_contract_series.ts
+++ scripts/deploy_contract_series.ts
@@ -1,6 +1,30 @@
+import Convert from "../utils/token-conversion";
 // STAGE 0
 // - accept custom params - via main
+
+
 // - establish a deployment series config w/ meta
+export const tokenTotalSupply = 1000000;
+export const deploymentConfig = {
+  contractConfigs: [
+    {
+      type: 'token_asset_manager',
+      _name: 'Chicken Scratch',
+      _symbol: 'SCRATCH',
+      _decimals: 18,
+      _totalSupply: tokenTotalSupply
+    },
+    {
+      type: 'ICO_sale_manager',
+      _name: 'Crowdsale',
+      _tokenContractAddress: 'TBD', // !
+      _maxTokens: tokenTotalSupply,
+      _price: Convert.TokensToWei('.005')
+    }
+  ]
+}
+
+
 // STAGE 1
 // - map on config serries
   // - collect contract
