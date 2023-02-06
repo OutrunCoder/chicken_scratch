6e072865904cfae10312b872a14a2f35043a8ac4
David Pineda <david.pineda@deparadise.com>
2023-02-06 18:35:30 -0500

    work pending on deployment series script

--- scripts/deploy_contract_series.ts
+++ scripts/deploy_contract_series.ts
@@ -5,31 +5,55 @@ import Convert from "../utils/token-conversion";
 
 // - establish a deployment series config w/ meta
 export const tokenTotalSupply = 1000000;
-export const deploymentConfig = {
-  contractConfigs: [
-    {
-      type: 'token_asset_manager',
-      _name: 'Chicken Scratch',
-      _symbol: 'SCRATCH',
-      _decimals: 18,
-      _totalSupply: tokenTotalSupply
-    },
-    {
-      type: 'ICO_sale_manager',
-      _name: 'Crowdsale',
-      _tokenContractAddress: 'TBD', // !
-      _maxTokens: tokenTotalSupply,
-      _price: Convert.TokensToWei('.005')
-    }
+export const contractConfigs = {
+  'TOKEN_ASSET_MANAGER': {
+    _name: 'Chicken Scratch',
+    _symbol: 'SCRATCH',
+    _decimals: 18,
+    _totalSupply: tokenTotalSupply
+  },
+  'ICO_SALE_MANAGER': {
+    _name: 'Crowdsale',
+    // _tokenContractAddress: 'TBD', // !
+    _maxTokens: tokenTotalSupply,
+    _price: Convert.TokensToWei('.005')
+  }
+};
+//
+const deploymentSchedule = {
+  stages: [
+    'DEPLOY-TOKEN_ASSET_MANAGER',
+    'DEPLOY-ICO_SALE_MANAGER',
+    'INITIALIZE-ICO_SALE'
   ]
 }
 
-
 // STAGE 1
-// - map on config serries
-  // - collect contract
-  // - Deploy w/ config params
+// - collect contract configs
+const deploymentReport = deploymentSchedule.stages.reduce((finalized, currentStage) => {
+  // - reduce on stage serries
+  let contract;
+  const [action, target] = currentStage.split('-');
+
+  if (action === 'DEPLOY') {
+    // - Deploy w/ config params
+
+  }
+  if (action === 'INITIALIZE') {
+
+  }
   // - confirm address via console
+
+  // continue
+  // return {
+  //   ...deployed,
+  //   [deploying.type]: {
+  //     ...deploying,
+  //     contract
+  //   } 
+  // }
+}, {});
+
   // next...
 // STAGE 3
 // - Call Business logic on contracts if required
