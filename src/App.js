5194c6c1ff147c63d1d00e16741531d089ec3cf1
David Pineda <david.pineda@deparadise.com>
2023-02-08 00:01:23 -0500

    Start base layout and nav header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderNavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <HeaderNavBar/>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
