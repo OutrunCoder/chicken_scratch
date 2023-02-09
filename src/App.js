87f0526ce6e618b77db6b2628f81aefcd746158c
David Pineda <david.pineda@deparadise.com>
2023-02-08 22:24:40 -0500

    connect FE to MetaMask via ethers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import logo from './logo.svg';
+
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
+
 import Navigation from './components/navigation';
 
+//
+
+//
+
 function App() {
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log('>> PROVIDER:', provider);
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    const account = ethers.utils.getAddress(accounts[0]);
+    console.log('>> ACCOUTNS:', account);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  });
+
   return (
     <Container>
       <div className="App">
