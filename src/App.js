f00b02377885643cd6f32a02a92d545023f860b2
David Pineda <david.pineda@deparadise.com>
2023-02-09 19:32:56 -0500

    integrate web3 config and pull contracts

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
+
+// FW
 import { useEffect, useState } from 'react';
+
+// assets
 import './App.css';
 import logo from './logo.svg';
 
+// libs
 import { Container } from 'react-bootstrap';
 import { ethers } from 'ethers';
 
+// components
 import Navigation from './components/navigation';
 import Info from './components/info';
 
-//
+// ABIs
+import TOKEN_ABI from './web3-config/abis/Token.json';
+import CROWDSALE_ABI from './web3-config/abis/Crowdsale.json';
 
-//
+// contract addresses
+import contractConfigs from './web3-config/contracts.json';
 
 function App() {
 
-  const [account, setAccount] = useState(null);
+  const networkConfig = contractConfigs['31337'];
+  const { token: tknConfig, crowdsale: crwdSacrwdSlConfig } = networkConfig;
 
+  console.log('>> WORKING CONFIGS:');
+  console.table({tknConfig, crwdSacrwdSlConfig});
+  
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
   // 
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    console.log('>> PROVIDER:', provider);
+    // Initiate provider
+    const _provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log('>> PROVIDER:', _provider);
+    setProvider(_provider);
 
+    // Initiate contracts
+    const tokenContract = new ethers.Contract(tknConfig.address, TOKEN_ABI, _provider);
+    const crowdsaleContract = new ethers.Contract(crwdSacrwdSlConfig.address, CROWDSALE_ABI, _provider);
+    // console.log('>> TOKEN CONTRACT:', tokenContract);
+
+    // Set Account
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
     const accountAddress = ethers.utils.getAddress(accounts[0]);
     console.log('>> ACCOUTNS:', accountAddress);
 
     setAccount(accountAddress);
+
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    loadBlockchainData();
-  });
+    if (isLoading) {
+      loadBlockchainData();
+    }
+  }, [isLoading]);
 
   return (
     <Container>
