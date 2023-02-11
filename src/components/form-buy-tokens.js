3bb68f70e3a6a5895150b4a3fcfde77c7a8ea8fa
David Pineda <david.pineda@deparadise.com>
2023-02-10 23:37:10 -0500

    add purchaseQty read for form handlers.

--- src/components/form-buy-tokens.js
+++ src/components/form-buy-tokens.js
@@ -1,11 +1,23 @@
-import { Button, Col, Form, Row } from "react-bootstrap";
-
+import { useState } from "react";
+import { Button, Form, Row } from "react-bootstrap";
 
 const BuyTokens = ({ provider, price, crowdContract, setIsLoading }) => {
+  const [purchaseQty, setPurchaseQty] = useState('0');
+
+
+  const buyHandler = async(e) => {
+    e.preventDefault();
+    console.log(`>> BUYING ${purchaseQty} TOKENS...`);
+  };
+
   return (
-    <Form style={{ maxWidth: '800px', margin: '50px auto' }}>
+    <Form onSubmit={buyHandler} style={{ maxWidth: '800px', margin: '50px auto' }}>
       <Form.Group as={Row}>
-          <Form.Control type="number" placeholder="Enter amount" className="my-4"/>
+          <Form.Control
+            type="number"
+            placeholder="Enter amount"
+            className="my-4"
+            onChange={(e) => setPurchaseQty(e.target.value)}/>
           <Button variant="primary" type="submit" style={{ width: '100%' }}>
             Buy Tokens
           </Button>
