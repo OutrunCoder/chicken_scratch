fb2987d8e29275e676b4f6a4aebd9290259229d0
David Pineda <david.pineda@deparadise.com>
2023-02-10 23:24:55 -0500

    Implement form layout

--- src/components/form-buy-tokens.js
+++ src/components/form-buy-tokens.js
@@ -1,5 +1,17 @@
-const BuyTokens = ({ provider, price, crowdContract, setIsLoading }) => {
+import { Button, Col, Form, Row } from "react-bootstrap";
 
+
+const BuyTokens = ({ provider, price, crowdContract, setIsLoading }) => {
+  return (
+    <Form style={{ maxWidth: '800px', margin: '50px auto' }}>
+      <Form.Group as={Row}>
+          <Form.Control type="number" placeholder="Enter amount" className="my-4"/>
+          <Button variant="primary" type="submit" style={{ width: '100%' }}>
+            Buy Tokens
+          </Button>
+      </Form.Group>
+    </Form>
+  );
 };
 
 export default BuyTokens;
